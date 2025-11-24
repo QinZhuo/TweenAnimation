1ce10538843c1df4f1120c50ee15c909335bd4c2
QinZhuo <qinzhuo_b@qq.com>
2025-11-25 05:55:14 +0800

    tween_method value

--- addons/TweenAnimation/TweenMethod.gd
+++ addons/TweenAnimation/TweenMethod.gd
@@ -1,7 +1,7 @@
 @tool
 class_name TweenMethod extends TweenValue
 
-signal tween_method
+signal tween_method(value)
 
 func _create_tweenr(tween: Tween):
 	_set_tweener_curve(tween.tween_method(tween_method.emit, _get_tween_from_value(), _get_tween_final_value(), _get_tween_duration()))
